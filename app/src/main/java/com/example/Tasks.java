35bdc098eb300b1ee1ebe83de342f076ebd48c00
mflavin <mflavin@hawk.iit.edu>
2018-04-29 16:43:29 -0500

    Added new files

--- app/src/main/java/com/example/Tasks.java
+++ app/src/main/java/com/example/Tasks.java
@@ -1,8 +1,42 @@
 package com.example;
 
-/**
- * Created by Flavin on 4/29/2018.
- */
-
 public class Tasks {
+    private int id;
+    private String day;
+    private String task;
+    private String timeofday;
+
+    public Tasks() {
+
+    }
+
+    public Tasks(String day, String task,String timeofday) {
+        this.day = day;
+        this.task=task;
+        this.timeofday=timeofday;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setDay(String day) {
+        this.day = day;
+    }
+    public void setTask(String task) {
+        this.task = task;
+    }
+    public void setTimeofday(String timeofday) {
+        this.timeofday = timeofday;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getDay() {
+        return day;
+    }
+    public String getTask() {
+        return task;
+    }
+    public String getTimeofday(){return timeofday;}
 }
--- app/src/main/java/com/example/calendarTasks.java
+++ app/src/main/java/com/example/calendarTasks.java
@@ -1,8 +1,39 @@
 package com.example;
 
-/**
- * Created by Flavin on 4/29/2018.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class calendarTasks {
+public class calendarTasks extends SQLiteOpenHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_Name = "CalendarDays";
+    // Calendar table name
+    private static final String TABLE_Calendar = "Calendar";
+    // Calendar Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_DAY = "day";
+    private static final String KEY_TASK = "task";
+    private static final String KEY_TIME = "timeofday";
+
+    public calendarTasks(Context context) {
+        super(context, DATABASE_Name, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_Calendar + "("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + KEY_DAY +
+                " TEXT, " + KEY_TASK + " TEXT, " + KEY_TIME + " TEXT "+ ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_Calendar);
+        // Creating tables again
+        onCreate(db);
+    }
 }
