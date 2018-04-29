41d7933f877ddc7af77595a48369ba731e9dca4c
mflavin <mflavin@hawk.iit.edu>
2018-04-29 16:47:42 -0500

    Added functions to add tasks to a day, get a specific days tasks, and remove a task.

--- app/src/main/java/com/example/calendarTasks.java
+++ app/src/main/java/com/example/calendarTasks.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class calendarTasks extends SQLiteOpenHelper {
     // Database Version
@@ -35,5 +40,54 @@ public class calendarTasks extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_Calendar);
         // Creating tables again
         onCreate(db);
+    }
+
+    // Adding new tasks on a calendar day
+    public void addTask(Tasks task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_DAY, task.getDay()); // Calendar Day
+        values.put(KEY_TASK, task.getTask()); // Name of the task
+        values.put(KEY_TIME,task.getTimeofday());// Time of day for the task
+
+        // Inserting Row
+        db.insert(TABLE_Calendar, null, values);
+        db.close(); // Closing database connection
+    }
+
+    // Getting All Tasks from on calendar day
+    public List<Tasks> getSpecificDay(String k) {
+        List<Tasks> TaskList = new ArrayList<Tasks>();
+
+        // Select All Tasks on a certain Calendar day
+        String selectQuery = "SELECT * FROM " + TABLE_Calendar + " WHERE " + KEY_DAY + " = " + k;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Tasks task = new Tasks();
+                task.setId(Integer.parseInt(cursor.getString(0)));
+                task.setDay(cursor.getString(1));
+                task.setTask(cursor.getString(2));
+                task.setTimeofday((cursor.getString(3)));
+                // Adding to list
+                TaskList.add(task);
+            } while (cursor.moveToNext());
+        }
+
+        // return list
+        return TaskList;
+    }
+
+    // Deleting a task
+    public void deleteTask(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_Calendar, KEY_ID + " = ?",
+                new String[]{String.valueOf(id)});
+        db.close();
     }
 }
